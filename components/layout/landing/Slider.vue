941903b7032b34e03dfe5f3cdb6247924ce19aa0
Krios <krios.mane@gmail.com>
2023-06-01 16:21:20 +0200

    wip

---
+++ components/layout/landing/Slider.vue
@@ -0,0 +1,85 @@
+<template>
+    <swiper :modules="modules" :mousewheel="true" :slides-per-view="1" :space-between="0" @swiper="onSwiper"
+        @slideChange="onSlideChange">
+
+
+        <swiper-slide v-for="(slide, index) in slides" :key="index">
+
+            <div class="sm:pb-24 sm:pt-32 xl:pb-32">
+
+                <div :class="'bg-slide-' + slide.id" class="pb-20 sm:pb-24 xl:pb-0">
+
+                    <div
+                        class="mx-auto flex max-w-8xl flex-col items-center gap-x-28 gap-y-10 px-10 sm:gap-y-8 lg:px-40 xl:flex-row xl:items-stretch">
+
+                        <div class="-mt-8 w-full max-w-2xl xl:-mb-8 xl:w-96 xl:flex-none">
+                            <div class="relative  h-full ">
+                                <img class="absolute inset-0 h-full w-full rounded-2xl  object-cover shadow-2xl"
+                                    :src="slide.image" />
+                            </div>
+                        </div>
+
+                        <div class="w-full max-w-2xl xl:max-w-none xl:flex-auto pt-12 pb-20 space-y-8">
+
+                            <h2 class="text-dark-blue font-bold text-[30px]">{{ slide.title }} </h2>
+
+                            <blockquote class="text-lg font-normal leading-[27px] text-black sm:text-lg sm:leading-[27px]">
+                                <p>
+                                    {{ slide.text }}
+                                </p>
+                            </blockquote>
+
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </swiper-slide>
+
+    </swiper>
+</template>
+
+
+<script setup>
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Mousewheel, Pagination } from 'swiper';
+
+
+// Import Swiper styles
+import 'swiper/css';
+
+import image_1 from "~/assets/images/slide_1.png";
+import image_2 from "~/assets/images/slide_2.png";
+import image_3 from "~/assets/images/slide_3.png";
+import image_4 from "~/assets/images/slide_4.png";
+
+
+
+const modules = [Mousewheel, Pagination];
+
+
+const slides = [
+
+
+    { id: 1, title: 'Cattura le migliori opportunità', image: image_1, text: 'Glintzo ti permette di trovare maggiori opportunità lavorative, nuovi clienti,mettendoti in contatto con aziende e privati non necessariamente locali, ampliando il tuo portfolio e affrontando sempre nuove sfide. Su Glintzo puoi lavorare: con chi vuoi, quando vuoi e da dove vuoi.' },
+    { id: 2, title: 'Metti in luce il tuo portfolio', image: image_2, text: 'Glintzo valorizza le tue competenze distintive, ancora prima di chi sei, della tua età, genere ed etnia. Crediamo che il tuo portfolio debba essere l\'informazione più importante che viene data su di te come Visual Creator per presentarti ad aziende e privati.' },
+    { id: 3, title: 'Inquadra le richieste dei clienti', image: image_3, text: 'Grazie a un\'interfaccia user friendly Glintzo permette una comunicazione semplice ed efficace con i clienti. Stiamo lavorando allo sviluppo delle Glintzo Guidelines per garantire processi trasparenti e contratti chiari, tutelando i creators da un lato e garantendo lavori di qualità dall\'altro.' },
+    { id: 4, title: 'Metti a fuoco il tuo business', image: image_4, text: 'Non è mai stato così facile salvaguardare i tuoi interessi come su Glintzo. Non dovrai più preoccuparti di come promuovere i tuoi lavori e di gestire le tue vendite. Glintzo lo farà per te, permettendoti di concentrarti sul tuo talento e di guadgnare senza alcuna preoccupazione.' },
+
+
+];
+
+/**
+ * 
+ */
+function onSwiper(swiper) {
+
+}
+
+/**
+ * 
+ */
+function onSlideChange() {
+
+}
+</script>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,12 @@ module.exports = {
     "./nuxt.config.{js,ts}",
     "./app.vue",
   ],
+  safelist: [
+    'bg-slide-1',
+    'bg-slide-2',
+    'bg-slide-3',
+    'bg-slide-4',
+  ],
   theme: {
     extend: {
       fontFamily: {
@@ -19,6 +25,13 @@ module.exports = {
         "dark-blue": "#02145A",
         pink: "#FB2283"
       },
+      backgroundImage: {
+        "slide-1" : "linear-gradient(270deg, rgba(34, 134, 251, 0.6) 0.13%, rgba(34, 134, 251, 0.4) 50.52%, rgba(34, 134, 251, 0.6) 100%)",
+        "slide-2" : "linear-gradient(270deg, rgba(251, 34, 131, 0.6) 0%, rgba(251, 34, 131, 0.4) 55.73%, rgba(251, 34, 131, 0.6) 100%)",
+        "slide-3" : "linear-gradient(90deg, rgba(252, 146, 46, 0.6) 0%, rgba(252, 146, 46, 0.4) 46.25%, rgba(252, 146, 46, 0.6) 100%)",
+        "slide-4" : "linear-gradient(90deg, rgba(50, 246, 253, 0.6) 2.47%, rgba(50, 246, 253, 0.4) 47.68%, rgba(50, 246, 253, 0.6) 96.44%)"
+
+      }
     },
     maxWidth: {
       "8xl": "90rem",
