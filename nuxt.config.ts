f5f76d835aeaf6740c53ea0b904a50dbc24713da
Krios <krios.mane@gmail.com>
2023-09-14 15:10:28 +0200

    wip

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,35 +1,36 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    
-    runtimeConfig: {
 
-        public: {
-            
-            BASE_URL : process.env.BASE_URL
+  runtimeConfig: {
 
-        }
-        
-    },
+    public: {
 
-    ssr: true,
-    buildModules: [
-        '@nuxtjs/google-fonts'
-    ],
+      BASE_URL: process.env.BASE_URL,
+      google_analytics_id: process.env.GOOGLE_ANALYTICS_ID
+
+    }
+
+  },
+
+  ssr: true,
+  buildModules: [
+    '@nuxtjs/google-fonts'
+  ],
   build: {
-    extend() {},
+    extend() { },
     transpile: ['gsap'],
+  },
+  css: ['~/assets/css/main.css'],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
     },
-    css: ['~/assets/css/main.css'],
-    postcss: {
-        plugins: {
-          tailwindcss: {},
-          autoprefixer: {},
-        },
-      },
-      googleFonts: {
-        families: {
-        
-          'Poppins': true,
-        }
-      }
+  },
+  googleFonts: {
+    families: {
+
+      'Poppins': true,
+    }
+  }
 })
