6132c946490c8f6014c6b6d2d77c7df794db31ee
Krios <krios.mane@gmail.com>
2023-09-14 12:04:36 +0200

    wip

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,21 +1,16 @@
 <template>
-    <div class="h-screen"></div>
+    <div class="h-screen">
+
+        <Script id="CookieDeclaration" type="text/javascript"
+            src="https://consent.cookiebot.com/904a8c3b-7ad0-4c82-ba50-fa2f1d5015be/cd.js" :async="true"></Script>
+
+
+
+    </div>
 </template>
 <script setup>
 definePageMeta({
     layout: "landing",
 });
-
-useHead({
-    title: 'Glintzo',
-    script: [
-        {
-            async: true,
-            src: 'https://consent.cookiebot.com/904a8c3b-7ad0-4c82-ba50-fa2f1d5015be/cd.js',
-            id: "CookieDeclaration",
-        },
-    ]
-})
-
 
 </script>
