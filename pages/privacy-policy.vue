7aa8b40a84810df3c6a74338e64f3d10bdb44aa1
Krios <krios.mane@gmail.com>
2023-09-14 12:07:15 +0200

    wip

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="w-full h-full min-h-screen">
-        <div class="max-w-8xl mx-auto px-10 sm:py-4 sm:px-20 xl:px-40"></div>
+        <div class="max-w-8xl mx-auto px-10 sm:py-4 sm:px-20 xl:px-40">
 
-        <Script id="CookieDeclaration" type="text/javascript"
-            src="https://consent.cookiebot.com/904a8c3b-7ad0-4c82-ba50-fa2f1d5015be/cd.js" :async="true"></Script>
+            <Script id="CookieDeclaration" type="text/javascript"
+                src="https://consent.cookiebot.com/904a8c3b-7ad0-4c82-ba50-fa2f1d5015be/cd.js" :async="true"></Script>
 
 
+        </div>
     </div>
-</div></template>
+</template>
 <script setup>
 definePageMeta({
     layout: "landing",
