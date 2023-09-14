8066ea0e747eb50e6ac672bb1db3463e22fe36e2
Krios <krios.mane@gmail.com>
2023-09-14 12:09:08 +0200

    wip

--- pages/privacy-policy.vue
+++ pages/privacy-policy.vue
@@ -4,8 +4,6 @@
 
             <Script id="CookieDeclaration" type="text/javascript"
                 src="https://consent.cookiebot.com/904a8c3b-7ad0-4c82-ba50-fa2f1d5015be/cd.js" :async="true"></Script>
-
-
         </div>
     </div>
 </template>
